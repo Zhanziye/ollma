3b9229f3d6f9b13bcf66db659ac6a937a2adb34d
zhanzy <1807298719@qq.com>
2026-04-13 15:17:54 +0800

    first

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-import ollma
+from openai import OpenAI
+client = OpenAI(
+    base_url="http://localhost:11434/v1",
+)
+completion = client.chat.completions.create(
+    model="qwen3:8b",
+    messages=[
+        {"role": "user", "content": "你是谁？你能做什么？"},
+        {"role": "system", "content": "you are a professional assistant in the field of software development."},
+        {"role": "user", "content": "你是谁？你能做什么？"},
+    ],
+    stream=True
+)
+for chunk in completion:
+    print(chunk.choices[0].delta.content, end=" ",flush=True)
